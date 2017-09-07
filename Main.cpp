f6e497025dc4bb094568b1b2c255b03a84f9e485
Johan Svensson <johan@exclude.se>
2017-09-07 20:00:55 +0200

    Initial settings.

--- Main.cpp
+++ Main.cpp
@@ -7,9 +7,34 @@
 
 #include "Main.h"
 
+#include <iostream>       // std::cout
+#include <thread>         // std::thread
+
+
+void mrf24jThread(){
+	std::cout << __FUNCTION__ << std::endl;
+}
+void ledThread(){
+	std::cout << __FUNCTION__ << std::endl;
+}
+void buttonThread(){
+	std::cout << __FUNCTION__ << std::endl;
+}
 
 int main(void){
 	//Start program:
 
+	//create mutexes here
+
+	//Create mrf24j:
+	std::thread mrf24j_thread(mrf24jThread);
+	//Create led:
+	std::thread led_thread(ledThread);
+	//Create button:
+	std::thread button_thread(buttonThread);
+
+	mrf24j_thread.join();
+	led_thread.join();
+	button_thread.join();
 
 }
